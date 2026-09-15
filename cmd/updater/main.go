704ea0c40d6297509a8e140ee82415750e177f8f
Cryosimorgh <80147430+Cryosimorgh@users.noreply.github.com>
2026-09-15 17:05:08 +0330

    Improve updater replacement safety

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -4,21 +4,39 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"time"
 )
 
 func main() {
 	target := flag.String("target", "BoltDM.exe", "application to replace")
 	source := flag.String("source", "BoltDM.exe.new", "new application")
+	restart := flag.String("restart", "BoltDM.exe", "application to restart")
 	flag.Parse()
 
+	backup := *target + ".old"
+	_ = os.Remove(backup)
+
 	for i := 0; i < 30; i++ {
-		if err := os.Rename(*source, *target); err == nil {
-			return
+		if err := os.Rename(*target, backup); err == nil {
+			break
 		}
 		time.Sleep(time.Second)
 	}
 
-	fmt.Fprintln(os.Stderr, "unable to replace application")
-	os.Exit(1)
+	if err := os.Rename(*source, *target); err != nil {
+		_ = os.Rename(backup, *target)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(*target); err != nil {
+		_ = os.Rename(backup, *target)
+		os.Exit(1)
+	}
+
+	if abs, err := filepath.Abs(*restart); err == nil {
+		_ = exec.Command(abs).Start()
+	}
 }
